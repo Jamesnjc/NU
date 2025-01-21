156d57d66cb8b4aadfd03dbe81baf183f75013fd
MyckoPar <myckopar100@gmail.com>
2025-01-21 15:06:02 +0800

    new update

--- src/app/Dashboard/Docu/page.tsx
+++ src/app/Dashboard/Docu/page.tsx
@@ -125,13 +125,13 @@ export default function Home() {
       >
         <Container maxWidth="lg">
           <Typography variant="h3" gutterBottom>
-            Why Choose Synx?
+            What is Synx?
           </Typography>
           <Typography variant="body1" paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </Typography>
           <Typography variant="h4" gutterBottom>
-            What is Synx?
+            Why choose Synx?
           </Typography>
           <Typography variant="body1" paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
