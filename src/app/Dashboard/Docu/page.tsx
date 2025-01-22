18d4114cd836d4954021a5b6d285d8ce655837f3
MyckoPar <111344149+MyckoPar@users.noreply.github.com>
2025-01-22 11:09:31 +0800

    Doneee

--- src/app/Dashboard/Docu/page.tsx
+++ src/app/Dashboard/Docu/page.tsx
@@ -117,7 +117,20 @@ export default function Home() {
           </List>
 
           <Typography variant="body1" paragraph>
-            Explore our documentation to learn more about how to use Synx and make the most of our platform.
+            
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+          </Typography>
+
+          
+          <Typography variant="body1" paragraph>
+            
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
+                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
           </Typography>
         </Container>
       </Box>
